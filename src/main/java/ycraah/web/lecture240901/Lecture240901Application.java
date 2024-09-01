e2a7b9ba498f41b8f15b0aeee256a0860d6eaebd
ycraah <ycraah@gmail.com>
2024-09-01 09:30:02 +0900

    setting

---
+++ src/main/java/ycraah/web/lecture240901/Lecture240901Application.java
@@ -0,0 +1,13 @@
+package ycraah.web.lecture240901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture240901Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Lecture240901Application.class, args);
+  }
+
+}
---
+++ src/test/java/ycraah/web/lecture240901/Lecture240901ApplicationTests.java
@@ -0,0 +1,13 @@
+package ycraah.web.lecture240901;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture240901ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
