80ef17d4b4247c6ca1c48b6ba8a12feb6c01e330
ycraah <ycraah@gmail.com>
2024-09-01 10:04:56 +0900

    getMapping연습:addPerson

---
+++ src/main/java/ycraah/web/lecture240901/controller/Homecontroller.java
@@ -0,0 +1,43 @@
+package ycraah.web.lecture240901.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/home")
+public class Homecontroller {
+  List<Person> people = new ArrayList<>();
+  int count = 0;
+
+  @GetMapping("/addPerson")
+  @ResponseBody
+  public String addPerson(String name, int age) {
+    count++;
+    Person person = new Person(count, name, age);
+    people.add(person);
+    return "응답 : %d번 사람이 추가되었습니다.".formatted(count);
+  }
+}
+
+
+
+@Getter
+@AllArgsConstructor
+class Person {
+  int id;
+  String name;
+  int age;
+}
+
